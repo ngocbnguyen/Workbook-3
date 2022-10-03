6b9e91d2b2cd4b1be148c023a4ca395f083052b5
Ngoc <ngocbnguyen19@gmail.com>
2022-10-03 11:19:06 -0400

    exercise 1

---
+++ Exercise /Functions/simple-functions.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function favouriteThings() {
+    console.log("my name is Ngoc.");
+    console.log("my favourite movie is Howl's Moving Castle.");
+    console.log("my favourite musician is merrymerryseoul.");
+}
+favouriteThings();
+
+function whyImHere() {
+    console.log("I'm here to learn to code.");
+}
+whyImHere();
+
+function favouritePlace() {
+    console.log("My favourite place to visit is Singapore because their food is so good!");
+}
+favouritePlace();
